f40f6b871f107991c25b2a85379e5752ff10bc66
lnace <35407136+2150689@users.noreply.github.com>
2019-06-07 18:47:11 +0100

    Update contacts.js
    Added ;'s

--- js/contacts.js
+++ js/contacts.js
@@ -24,7 +24,7 @@
             }
         });
         //Make it available again
-        $select.removeAttr('disabled')
+        $select.removeAttr('disabled');
     }
     
     window.onload = function () {
