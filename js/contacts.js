6ea9a96010c046bc70212b054c2e1dcf78fc07a2
lnace <35407136+2150689@users.noreply.github.com>
2019-06-08 12:44:46 +0100

    org.openqa.selenium.WebDriverException: com.gargoylesoftware.htmlunit.ScriptException: missing ; before statement (http://35.246.92.202/js/contacts.js#31) Still attempting HtmlUnit.

--- js/contacts.js
+++ js/contacts.js
@@ -24,7 +24,7 @@
             }
         });
         //Make it available again
-        $select.removeAttr('disabled')
+        $select.removeAttr('disabled');
     }
 
     function filterBySource (data, $buttonSource, source) {
